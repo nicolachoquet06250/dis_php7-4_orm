0a9040fb08a3683089159bf6ed91a537ad319a7f
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2020-03-13 14:19:54 +0100

    ajout d'une commande 'migrate'

---
+++ lib/classes/commands/Migrate.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace dis\orm\classes\commands;
+
+
+use dis\core\traits\Command;
+
+class Migrate {
+    use Command;
+
+    public function database() {
+
+    }
+
+    public function help() {}
+}
---
+++ lib/classes/commands/Register.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace dis\orm\classes\commands;
+
+
+class Register extends \dis\core\classes\commands\Register {
+    public static function set_commands() {
+        static::$commands = [
+            ...static::$commands,
+            'migrate' => Migrate::class
+        ];
+    }
+}
