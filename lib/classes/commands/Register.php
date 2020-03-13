d778e4ca711d1f2ed3ebd15586d5e9a761b4e88a
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2020-03-13 14:42:04 +0100

    ajout d'une commande 'migrate'

--- lib/classes/commands/Register.php
+++ lib/classes/commands/Register.php
@@ -7,9 +7,8 @@ namespace dis\orm\classes\commands;
 class Register extends \dis\core\classes\commands\Register {
     public static function set_commands() {
         parent::set_commands();
-        static::$commands = [
-            ...static::$commands,
+        static::$commands = array_merge(static::$commands, [
             'migrate' => Migrate::class
-        ];
+        ]);
     }
 }
